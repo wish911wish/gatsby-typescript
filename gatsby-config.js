6a4b78d5a6cee48b610aceda3e1c85bc24e6517e
wish911wish <wish911wish@gmail.com>
2020-10-14 22:55:23 +0900

    fix gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
     siteMetadata: {
         title: `Gatsby Default Starter`,
@@ -27,6 +29,13 @@ module.exports = {
                 icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
             },
         },
+        {
+            resolve: 'gatsby-plugin-root-import',
+            options: {
+                src: path.join(__dirname, 'src'),
+                pages: path.join(__dirname, 'src/pages')
+            }
+        }
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
